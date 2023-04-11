bf7584a7f28328db59b6468d61af0dd38f9eb4fa
Zkirvaldize <giorgi.kirvalidze@georgiatech.eu>
2023-04-11 14:54:27 +0400

    added docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ConfigModule} from '@nestjs/config';
+import {DatabaseModule} from './database/database.module';
+import * as Joi from "joi";
+
 
 @Module({
-  imports: [],
+  imports: [
+    // ConfigModule.forRoot({
+    //   validationSchema: Joi.object({
+    //     MYSQL_HOST: Joi.string().required(),
+    //     MYSQL_PORT: Joi.number().required(),
+    //     MYSQL_USER: Joi.string().required(),
+    //     MYSQL_PASSWORD: Joi.string().required(),
+    //     MYSQL_DB: Joi.string().required(),
+    //   }),
+    // }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
